8d98c86cceca168edd34a6f77da541bfba81c93a
byspor <slokvami@mail.ru>
2025-02-17 09:58:57 +0300

    прочитал про NuGet

--- AssembliesAndNamespaces/Program.cs
+++ AssembliesAndNamespaces/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+using static System.Console;
+
+Write("Enter a color value in hex: ");
+string? hex = ReadLine(); // или "00ffc8"
+WriteLine("Is {0} a valid color value? {1}", arg0: hex, arg1: hex.IsValidHex());
+
+Write("Enter a XML element: ");
+string? xmlTag = ReadLine(); // или "<h1 class=\"<\" />"
+WriteLine("Is {0} a valid XML element? {1}", arg0: xmlTag, arg1: xmlTag.IsValidXmlTag());
+
+Write("Enter a password: ");
+string? password = ReadLine(); // или "secretsauce"
+WriteLine("Is {0} a valid password? {1}", arg0: password, arg1: password.IsValidPassword());
---
+++ SharedLibrary/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Packt.Shared
+{
+    public static class StringExtensions
+    {
+        public static bool IsValidXmlTag(this string input)
+        {
+            return Regex.IsMatch(input, @"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$");
+        }
+
+        public static bool IsValidPassword(this string input)
+        {
+            // не менее восьми допустимых символов
+            return Regex.IsMatch(input, "^[a-zA-Z0-9_-]{8,}$");
+        }
+
+        public static bool IsValidHex(this string input)
+        {
+            // три или шесть допустимых шестнадцатеричных символов
+            return Regex.IsMatch(input, "^#?([a-fA-F0-9]{3}|[a-fA-F0-9]{6})$");
+        }
+    }
+}
+
